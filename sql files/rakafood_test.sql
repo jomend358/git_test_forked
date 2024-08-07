a07c5494483f063915dcb989e21b4f53011a37f6
Jomen034 <jomenpardede@gmail.com>
2024-08-07 11:32:30 +0700

    update query: add value to filter

--- sql files/rakafood_test.sql
+++ sql files/rakafood_test.sql
@@ -1,5 +1,5 @@
 select *
 from public.rakamin_customer_address
-where lower(kota) in ('jakarta timur', 'jakarta utara')
+where lower(kota) in ('jakarta timur', 'jakarta utara', 'jakarta barat')
 	and (alamat = 'TMII' or alamat = 'Jatinegara' or alamat = 'Kalideres')
 ;
