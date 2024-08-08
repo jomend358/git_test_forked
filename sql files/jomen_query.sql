7969982b92c080318cd46e2beaa24af16dd9c3df
Jomen034 <jomenpardede@gmail.com>
2024-08-08 09:28:38 +0700

    update query model 13th

--- sql files/jomen_query.sql
+++ sql files/jomen_query.sql
@@ -2,6 +2,6 @@ SELECT *
 FROM public.rakamin_customer_address
 WHERE 
 	LOWER(kota) IN ('jakarta timur', 'jakarta utara', 'jakarta barat')
-	AND (alamat = 'TMII' OR alamat = 'Jatinegara' OR alamat = 'Kalideres')
+	-- AND (alamat = 'TMII' OR alamat = 'Jatinegara' OR alamat = 'Kalideres')
 	AND user_name IS NOT NULL
 ;
